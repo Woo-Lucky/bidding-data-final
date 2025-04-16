e9705fe0352aa2ee8c475c2ba6cdcb9bc4a96994
Woo_Lucky <dldngur8581@gmail..com>
2025-04-16 09:14:03 +0900

    vscode commit

--- api/index.ts
+++ api/index.ts
@@ -28,7 +28,7 @@ app.get("/bidding", function (req: any, res: any) {
 
 app.listen(3000, function () {
   console.log(
-    //"http://127.0.0.1:3000/bidding?serviceKey=snOlXoqmO%2BAuj39eC%2BRSnF27Y7g8dM%2Bg4YxudtLaq5664xOw1XdFW6JMGQE1Fi%2F3981bQGnKsooawG67Jn0zTg%3D%3D&inqryDiv=1&inqryBgnDt=202504110000&inqryEndDt=202504112359&pageNo=1&numOfRows=10&bidNtceNm= app listening on port 3000!"
-    "http://127.0.0.1:3000/bidding?serviceKey=snOlXoqmO%2BAuj39eC%2BRSnF27Y7g8dM%2Bg4YxudtLaq5664xOw1XdFW6JMGQE1Fi%2F3981bQGnKsooawG67Jn0zTg%3D%3D&inqryDiv=1&pageNo=1&numOfRows=9999 app listening on port 3000!"
+    "http://127.0.0.1:3000/bidding?serviceKey=snOlXoqmO%2BAuj39eC%2BRSnF27Y7g8dM%2Bg4YxudtLaq5664xOw1XdFW6JMGQE1Fi%2F3981bQGnKsooawG67Jn0zTg%3D%3D&inqryDiv=1&inqryBgnDt=202504110000&inqryEndDt=202504112359&pageNo=1&numOfRows=10&bidNtceNm= app listening on port 3000!"
+    //"http://127.0.0.1:3000/bidding?serviceKey=snOlXoqmO%2BAuj39eC%2BRSnF27Y7g8dM%2Bg4YxudtLaq5664xOw1XdFW6JMGQE1Fi%2F3981bQGnKsooawG67Jn0zTg%3D%3D&inqryDiv=1&pageNo=1&numOfRows=9999 app listening on port 3000!"
   );
 });
